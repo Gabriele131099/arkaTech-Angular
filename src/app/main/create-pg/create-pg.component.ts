68daff400348127eb8cc78bd2e51ec3b5d5ad5b8
Gabriele131099 <gabrisanna131099@gmail.com>
2023-03-23 00:28:40 +0100

    form create pg

--- src/app/main/create-pg/create-pg.component.ts
+++ src/app/main/create-pg/create-pg.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { RacesService } from 'src/app/service/races.service';
 
 @Component({
@@ -8,18 +9,39 @@ import { RacesService } from 'src/app/service/races.service';
 })
 export class CreatePgComponent {
   constructor(private races:RacesService) { }
+  
+  createPgForm = new FormGroup({
+    characterName: new FormControl('', [Validators.required]),
+    race: new FormControl('', [Validators.required]),
+
+    alignment: new FormControl('', [Validators.required]),
+    background: new FormControl('', [Validators.required]),
+    personalityTraits: new FormControl('', [Validators.required]),
+    ideals: new FormControl('', [Validators.required]),
+    bonds: new FormControl('', [Validators.required]),
+    flaws: new FormControl('', [Validators.required]),
+    raceAndFeatures: new FormControl('', [Validators.required]),
+    featuresAndTraits: new FormControl('', [Validators.required]),
+    aClass: new FormControl('', [Validators.required]),
+
+  });
   arrayRace:String[] = this.races.arrayRaces
-
+  positionForm:number=0;
+  next(){
+    this.positionForm++
+  }
+  back(){
+    this.positionForm--
+  }
   createPg(){
+    /** Usage returns typed data */
+    const data = fetch(`http://localhost:8080/pg`, {
+      method: "POST",
+      body: "Simon" ,
+    }).then((res) => {
+      window.location.href="http://localhost:4200/archivio"
 
-/** Usage returns typed data */
-const data = fetch(`http://localhost:8080/pg`, {
-  method: "POST",
-  body: "Simon" ,
-}).then((res) => {
-  window.location.href="http://localhost:4200/archivio"
-
-  console.log(res)
-});
+      console.log(res)
+    });
   }
 }
